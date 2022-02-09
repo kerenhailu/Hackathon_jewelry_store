bc8beed64548c6e1bc4b7543441c1b999161e2e4
haimaye074@gmail.com <haimaye074@gmail.com>
2022-02-10 00:02:38 +0200

    changes

--- src/AppRouter/AppRouter.component.jsx
+++ src/AppRouter/AppRouter.component.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter,Route, Routes } from "react-router-dom"
 
 
 const AppRouter=()=>{
@@ -5,7 +6,11 @@ const AppRouter=()=>{
         <div>
             <h1>AppRouter</h1>
             <BrowserRouter>
+            <Routes>
+            <Route exact path="/" element={<Home/>} />
+            </Routes>
             </BrowserRouter>
         </div>
     )
 }
+export default AppRouter;
