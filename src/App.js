39e10db5213d8b936aaf3c108621c03934c7619d
kerenhailu <keren4509@gmail.com>
2022-02-10 00:09:50 +0200

    47

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/Parts/NavBar/NavBar.Component";
+import AppRouter from "./AppRouter/AppRouter.component";
+
 
 function App() {
   return (
     <div className="App">
+      <AppRouter/>
       <NavBar />
     </div>
   );
