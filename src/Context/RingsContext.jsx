13f3e0995808039a567a5aa733ea8695158b7095
haimaye074@gmail.com <haimaye074@gmail.com>
2022-02-09 21:21:25 +0200

    Context

---
+++ src/Context/RingsContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useEffect, useState } from "react";
+
+
+const RingsContext=createContext();
+
+const RingsContextProvider=({Children})=>{
+    const [rings,setRings]=useState(RingsContext);
+    useEffect(()=>{
+    GetRingsData().then((res)=>setRings(res.RingsList))
+    },[])
+    return(
+    <RingsContext.Provider value={{rings,setRings}}>
+        {Children}
+    </RingsContext.Provider>
+    )
+}
+export default RingsContextProvider;
