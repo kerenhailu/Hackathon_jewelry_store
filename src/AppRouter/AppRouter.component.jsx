ab26af4fd97cb1121723035c1bcfad5d7927a884
haimaye074@gmail.com <haimaye074@gmail.com>
2022-02-09 23:44:34 +0200

    changes

---
+++ src/AppRouter/AppRouter.component.jsx
@@ -0,0 +1,11 @@
+
+
+const AppRouter=()=>{
+    return(
+        <div>
+            <h1>AppRouter</h1>
+            <BrowserRouter>
+            </BrowserRouter>
+        </div>
+    )
+}
---
+++ src/Components/UserForm/UserForm.jsx
@@ -0,0 +1,19 @@
+
+
+const UserForm=()=>{
+    return(
+        <div>
+            <form>
+                <label>first name:</label>
+                <input type="text"></input>
+                <label>last name:</label>
+                <input type="text"></input>
+                <label>email</label>
+                <input type="email"></input>
+                <label>password</label>
+                <input type="password"></input>
+                <button>send</button>
+            </form>
+        </div>
+    )
+}
