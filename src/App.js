52a64a68f1c9a51e93860c17532e7b9089b0eaa3
haimaye074@gmail.com <haimaye074@gmail.com>
2022-02-09 21:03:56 +0200

    555

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+        ////
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -23,3 +24,4 @@ function App() {
 }
 
 export default App;
+////CHECK////
