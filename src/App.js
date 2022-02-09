2f73a76740c94577b9767fb172a057a222e1c716
kerenhailu <keren4509@gmail.com>
2022-02-10 00:24:35 +0200

    77

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Components/Pages/Home/Home.Component"
 import NavBar from "./Components/Parts/NavBar/NavBar.Component";
-import AppRouter from "./AppRouter/AppRouter.component";
+// import NavBar from "./Components/Parts/NavBar/NavBar.Component";
+// import AppRouter from "./AppRouter/AppRouter.component";
+
 
 
 function App() {
   return (
     <div className="App">
-      <AppRouter/>
+      <h1>App</h1>
+      <Home/>
       <NavBar />
     </div>
   );
--- src/AppRouter/AppRouter.component.jsx
+++ src/AppRouter/AppRouter.component.jsx
@@ -1,17 +1,16 @@
-import { BrowserRouter,Route, Routes } from "react-router-dom"
-import Home from "../Components/Pages/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../Components/Pages/Home";
 
-
-const AppRouter=()=>{
-    return(
-        <div>
-            <h1>AppRouter</h1>
-            <BrowserRouter>
-            <Routes>
-            <Route exact path="/" element={<Home/>} />
-            </Routes>
-            </BrowserRouter>
-        </div>
-    )
-}
+const AppRouter = () => {
+  return (
+    <div>
+      <h1>AppRouter</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 export default AppRouter;
